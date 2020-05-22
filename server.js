4d594ec8f82f1b2c0a084faa63b9e626820a696d
maxwellhankner <maxwellhankner@gmail.com>
2020-05-22 17:17:19 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const port = process.env.PORT || 8000;
+const app = express();
+// const path = require('path');
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('build'));
+// }
+
+app.get('/api/design', (req, res) => {
+    const design = {
+        model: 'air force 1'
+    }
+    res.json(design)
+})
+
+// app.get('/*', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'))
+// })
+
+app.listen(port, () => {
+    console.log('App is listening on port:', port);
+})
