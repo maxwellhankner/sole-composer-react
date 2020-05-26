fef00cbef9063c265aa0196c187340b1252af578
maxwellhankner <maxwellhankner@gmail.com>
2020-05-26 17:29:29 -0500

    three.js working

--- client/src/components/Scene/Scene.js
+++ client/src/components/Scene/Scene.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Scene.css';
+import * as THREE from "three";
 
 function Scene() {
+
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        const width = canvasRef.current.clientWidth;
+        const height = canvasRef.current.clientHeight;
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, width/height, 0.1, 1000);
+        const renderer = new THREE.WebGLRenderer();
+
+        renderer.setSize(width, height);
+        renderer.setClearColor('#eeeeee');
+
+        const geometry = new THREE.BoxGeometry(1, 1, 1);
+        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        const cube = new THREE.Mesh(geometry, material);
+
+        scene.add(cube);
+
+        camera.position.z = 5;
+
+        renderer.render(scene, camera);
+
+        canvasRef.current.appendChild(renderer.domElement);
+
+    }, [])
+
     return (
-        <div className="scene-container">
-            Scene
-        </div>
+        <div className="scene-container" ref={canvasRef} />
     )
 }
 
