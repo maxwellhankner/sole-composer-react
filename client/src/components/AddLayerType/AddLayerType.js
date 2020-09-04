2609ab46129a0b76090046ec84f016685d62f7f9
themanmaran <tyler.maran@gmail.com>
2020-09-04 14:17:12 -0700

    Refactor LayersView to use domponentDictionary

--- client/src/components/AddLayerType/AddLayerType.js
+++ client/src/components/AddLayerType/AddLayerType.js
@@ -2,34 +2,36 @@ import React from 'react';
 import './AddLayerType.css';
 import { layerTypes } from '../../helpers/partsObject';
 
-function AddLayerType({ handleAddLayer, setLayersView, currentPartName }) {
-
+function AddLayerType({ props }) {
+	const { currentPartName, handleAddLayer, setLayersView } = props;
 	return (
 		<div className="design-preview-container">
-			<div className='view-title'>
+			<div className="view-title">
 				<p>Select Layer Type</p>
 			</div>
-			{layerTypes[currentPartName].types.map((type, i) =>
-				<div key={i} className='change-view-button'>
-					{
-						type === 'Mask' ?
-							<button onClick={() => {
+			{layerTypes[currentPartName].types.map((type, i) => (
+				<div key={i} className="change-view-button">
+					{type === 'Mask' ? (
+						<button
+							onClick={() => {
 								setLayersView('MaskType');
-							}}>
-								{type}
-							</button>
-							:
-							<button onClick={() => {
+							}}
+						>
+							{type}
+						</button>
+					) : (
+						<button
+							onClick={() => {
 								handleAddLayer(type);
-								setLayersView('Layers');
-							}}>
-								{type}
-							</button>
-					}
+								setLayersView('LayerOverview');
+							}}
+						>
+							{type}
+						</button>
+					)}
 				</div>
-	)
-}
-		</div >
+			))}
+		</div>
 	);
 }
 
--- client/src/components/ColorPicker/ColorPicker.js
+++ client/src/components/ColorPicker/ColorPicker.js
@@ -4,71 +4,109 @@ import { handleConvertPartName } from '../../helpers/convertPartNames';
 import CurrentColors from '../CurrentColors/CurrentColors';
 import CustomColor from '../CustomColor/CustomColor';
 
-function ColorPicker({ currentPartName, currentLayer, design, setLayersView, handleDesignChangeManager }) {
+function ColorPicker({ props }) {
+	const {
+		currentLayer,
+		currentPartName,
+		design,
+		handleDesignChangeManager,
+		setLayersView,
+	} = props;
 
-  const [currentColor, setCurrentColor] = useState('#ffffaa');
+	const [currentColor, setCurrentColor] = useState('#ffffaa');
+	const [colorsArray, setColorsArray] = useState([]);
 
-  const [colorsArray, setColorsArray] = useState([]);
+	useEffect(() => {
+		const getDesignColors = () => {
+			const colors = [];
+			for (const property in design.parts) {
+				for (const layer in design.parts[property].layers) {
+					if (design.parts[property].layers[layer].type === 'color') {
+						const thisColor =
+							design.parts[property].layers[layer].color;
+						if (!colors.includes(thisColor)) {
+							colors.push(thisColor);
+						}
+					}
+				}
+			}
+			return colors;
+		};
 
-  useEffect(() => {
-    
-    const getDesignColors = () => {
-      const colors = []
-      for (const property in design.parts) {
-        for (const layer in design.parts[property].layers) {
-  
-          if (design.parts[property].layers[layer].type === 'color') {
-            const thisColor = design.parts[property].layers[layer].color;
-            if (!colors.includes(thisColor)) {
-              colors.push(thisColor);
-            }
-  
-          }
-        }
-      }
-      return colors;
-    }
+		setColorsArray(getDesignColors());
 
-    setColorsArray(getDesignColors());
+		if (
+			currentPartName === 'outerOverlay' ||
+			currentPartName === 'innerOverlay'
+		) {
+			setCurrentColor(
+				design.overlays[currentPartName].layers[currentLayer].color
+			);
+		} else {
+			setCurrentColor(
+				design.parts[currentPartName].layers[currentLayer].color
+			);
+		}
+	}, [design, currentPartName, currentLayer, setColorsArray]);
 
-    if (currentPartName === 'outerOverlay' || currentPartName === 'innerOverlay') {
-      setCurrentColor(design.overlays[currentPartName].layers[currentLayer].color);
-    }
-    else {
-      setCurrentColor(design.parts[currentPartName].layers[currentLayer].color);
-    }
-  }, [design, currentPartName, currentLayer, setColorsArray]);
+	const handleColorChangeComplete = (color) => {
+		setCurrentColor(color);
+		handleDesignChangeManager([
+			'color-changed',
+			currentPartName,
+			currentLayer,
+			color.hex,
+		]);
+	};
 
-  const handleColorChangeComplete = (color) => {
-    setCurrentColor(color);
-    handleDesignChangeManager(['color-changed', currentPartName, currentLayer, color.hex]);
-  }
+	const handleColorChange = (color) => {
+		setCurrentColor(color);
+		handleDesignChangeManager([
+			'color-changed',
+			currentPartName,
+			currentLayer,
+			color,
+		]);
+	};
 
-  const handleColorChange = (color) => {
-    setCurrentColor(color);
-    handleDesignChangeManager(['color-changed', currentPartName, currentLayer, color]);
-  }
-
-
-  return (
-    <div className="color-picker-container">
-      <div className='view-title'>
-        <p>{handleConvertPartName(currentPartName)}</p>
-      </div>
-      <div className='random-color'>
-        <button onClick={() => handleColorChange('#' + Math.floor(Math.random() * 16777215).toString(16))}>Random Color</button>
-      </div>
-      <div>
-        <CurrentColors colorsArray={colorsArray} handleColorChange={handleColorChange} />
-      </div>
-      <div>
-        <CustomColor color={currentColor} onChangeComplete={handleColorChangeComplete} />
-      </div>
-      <div className='change-view-button'>
-        <button onClick={() => setLayersView('Layers')}>Back</button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="color-picker-container">
+			<div className="view-title">
+				<p>{handleConvertPartName(currentPartName)}</p>
+			</div>
+			<div className="random-color">
+				<button
+					onClick={() =>
+						handleColorChange(
+							'#' +
+								Math.floor(Math.random() * 16777215).toString(
+									16
+								)
+						)
+					}
+				>
+					Random Color
+				</button>
+			</div>
+			<div>
+				<CurrentColors
+					colorsArray={colorsArray}
+					handleColorChange={handleColorChange}
+				/>
+			</div>
+			<div>
+				<CustomColor
+					color={currentColor}
+					onChangeComplete={handleColorChangeComplete}
+				/>
+			</div>
+			<div className="change-view-button">
+				<button onClick={() => setLayersView('LayerOverview')}>
+					Back
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default ColorPicker;
