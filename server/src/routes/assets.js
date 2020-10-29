24c1e0134d227d500062b9c9bb67b50b0fb5e3b3
maxwellhankner <maxwellhankner@gmail.com>
2020-10-28 19:27:48 -0500

    multer route working

--- server/src/routes/assets.js
+++ server/src/routes/assets.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const { getModel } = require('../controllers/models');
-const { getImage } = require('../controllers/images');
+const { getImage, uploadImage } = require('../controllers/images');
 
 router.route('/models/af1').get(getModel);
 
 router.route('/images/:id').get(getImage);
+
+router.route('/uploadimage').post(uploadImage);
 
 module.exports = router;
