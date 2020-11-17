07471c2485e94e09da4f2071191f26fd3f102e93
maxwellhankner <maxwellhankner@gmail.com>
2020-11-16 18:40:41 -0600

    git ignore production

---
+++ client/build/precache-manifest.4606df24ccab9501084377e06f943b28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93c3451b30ffaee20422f38fdf6c4889",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e41eab473e36266f6e2",
+    "url": "/static/css/main.a431bb90.chunk.css"
+  },
+  {
+    "revision": "06864a629375a0abcbf4",
+    "url": "/static/js/2.5fdbac31.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.5fdbac31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e41eab473e36266f6e2",
+    "url": "/static/js/main.1b18519f.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
