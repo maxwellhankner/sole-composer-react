7de044fb07959db93c71c6ad661ed74773d3b603
themanmaran <tyler.maran@gmail.com>
2020-11-16 19:13:56 -0500

    getting ready for launch

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -1,31 +1,35 @@
 const User = require('../models/user');
 // const passport = require('passport');
 const passport = require('../middleware/googleAuth');
+const production = process.env.NODE_ENV === 'production';
+const redirectUrl = production
+	? process.env.URL
+	: `http://localhost:${process.env.PORT}/`;
 
 exports.getSession = async (req, res, next) => {
-  if (req.session.passport) {
-    const user = await User.findById(req.session.passport.user);
-    if (user) {
-      return res.status(200).json(user);
-    }
-    return res.status(401).json({ err: 'No user found' });
-  }
-  return res.status(200).json({ err: 'No Session' });
+	if (req.session.passport) {
+		const user = await User.findById(req.session.passport.user);
+		if (user) {
+			return res.status(200).json(user);
+		}
+		return res.status(401).json({ err: 'No user found' });
+	}
+	return res.status(200).json({ err: 'No Session' });
 };
 
 exports.google = (req, res, next) => {
-  passport.authenticate('google', { scope: ['profile', 'email'] });
+	passport.authenticate('google', { scope: ['profile', 'email'] });
 };
 
 exports.googleCallback = (req, res, next) => {
-  passport.authenticate('google', {
-    successRedirect: 'http://localhost:3000/',
-    failureRedirect: '/auth/login/failed',
-  });
+	passport.authenticate('google', {
+		successRedirect: redirectUrl,
+		failureRedirect: '/auth/login/failed',
+	});
 };
 
 exports.logout = (req, res, next) => {
-  req.logout();
-  req.session = null;
-  res.redirect('http://localhost:3000/');
+	req.logout();
+	req.session = null;
+	res.redirect(redirectUrl);
 };
