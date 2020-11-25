606493d0a38f673b40af6471ff7f0b1402659bbc
maxwellhankner <maxwellhankner@gmail.com>
2020-11-24 21:04:17 -0600

    test

--- client/src/pages/Designer/Designer.js
+++ client/src/pages/Designer/Designer.js
@@ -24,6 +24,7 @@ function Designer() {
       simpleFetch(`/api/outlines/${id}`, 'GET')
         .then((res) => res.json())
         .then((data) => {
+          console.log(data);
           setDesignSpec(data);
         });
     } else {
