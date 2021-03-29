9597a1025fe3a9f9237a1b27bc62e6227a536994
Maxwell Hankner <maxwellhankner@gmail.com>
2021-03-28 20:44:31 -1000

    sentry on

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import * as Sentry from '@sentry/react';
-// import { Integrations } from '@sentry/tracing';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 
-// Sentry.init({
-//   dsn:
-//     'https://d98f16c7cc0f4230b7ba30f6a76ef259@o484792.ingest.sentry.io/5538538',
-//   integrations: [new Integrations.BrowserTracing()],
-//   tracesSampleRate: 1.0,
-// });
+Sentry.init({
+  dsn:
+    'https://d98f16c7cc0f4230b7ba30f6a76ef259@o484792.ingest.sentry.io/5538538',
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
