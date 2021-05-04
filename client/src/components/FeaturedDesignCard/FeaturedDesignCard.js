78c02787313f59f10f82114029a2017970640572
Maxwell Hankner <maxwellhankner@gmail.com>
2021-05-03 14:27:30 -1000

    non users

--- client/src/components/FeaturedDesignCard/FeaturedDesignCard.js
+++ client/src/components/FeaturedDesignCard/FeaturedDesignCard.js
@@ -2,26 +2,24 @@ import React from 'react';
 import './FeaturedDesignCard.css';
 import { useHistory } from 'react-router-dom';
 
-function FeaturedDesignCard({ props, userData }) {
+function FeaturedDesignCard({ props }) {
   const { _id, title, configId, author, screenshot } = props;
 
   const history = useHistory();
 
   const handleClick = () => {
-    if (userData) {
-      history.push(`/designer/${_id}`);
-    }
+    history.push(`/designer/${_id}`);
   };
 
   return (
-    <div className='featureddesigncard-container' onClick={() => handleClick()}>
-      <div className='featureddesigncard-image'>
-        <img src={`api/assets/images/${screenshot}`} alt='feature-design' />
+    <div className="featureddesigncard-container" onClick={() => handleClick()}>
+      <div className="featureddesigncard-image">
+        <img src={`api/assets/images/${screenshot}`} alt="feature-design" />
       </div>
 
-      <p className='featureddesigncard-title'>{title}</p>
-      <p className='featureddesigncard-model'>{configId.modelName}</p>
-      <p className='featureddesigncard-author'>{author.firstName}</p>
+      <p className="featureddesigncard-title">{title}</p>
+      <p className="featureddesigncard-model">{configId.modelName}</p>
+      <p className="featureddesigncard-author">{author.firstName}</p>
     </div>
   );
 }
